3c407ea759dc048900f94c8b1a1cb3b3abefa19c
J_a_l_i_s <124979469+Jalismahamud@users.noreply.github.com>
2024-10-17 08:42:35 +0600

    Add user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            [
+                'name' => 'Admin',
+                'email' => 'admin@admin.com',
+                'password' => Hash::make('12345678')
+            ],
+            //    [
+            //      'name' => 'Client',
+            //     'email' => 'client@client.com',
+            //     'password' => Hash::make('12345678')
+            //    ],
+            //    [
+            //      'name' => 'tax-prepare',
+            //     'email' => 'tax-prepare@tax-prepare.com',
+            //     'password' => Hash::make('12345678')
+            //    ],
+
+        ]);
+    }
+}
